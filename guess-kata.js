4ee4a6578c02be300007a7bae2332d39bf8cbdf0
Anastasiia809 <anastasiia208805@student-schoolsofwestfield.org>
2025-04-03 14:57:10 +0000

    almost done

--- guess-kata.js
+++ guess-kata.js
@@ -1,16 +1,21 @@
 /* While Loop Review: Three Guesses */
 
 // Define a variable answer, set equal to a random number between 1 and 10
+var answer = Math.floor(Math.random()*10)+1;
 // Define a variable guess, set equal to 0
+var guess = 0;
 // Define a variable guessCount, set equal to 0
 // Let the user guess WHILE guessCount is less than 4
-// Add one to guessCount
+for (let guessCount = 0; guessCount < 4; guessCount++) {
 // Set guess equal to the user input after "Guess my number"
+    guess = prompt("Guess my number");
 // If guess is equal to answer, say "Right!" and break the loop
+    if (guess == answer) {
+        alert("Right!");
+        break;
+    }
 // Otherwise say "Wrong!"
+else alert("Wrong!");
 // End the loop
-// If guessCount is equal to 4, say "Out of guesses. The number was ANSWER"
-
-/* For Loop Conversion */
-
-// Rewrite this as a for loop. 
+if (guessCount == 3) alert("Out of guesses. The number was " + answer);
+}
--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+/* Multiplication */
 
+// Create 2 variables, a & b, and set to ne random integers between 3 and 9
+// Set a variable called equation to display  a * b = ??
+var question = 0;
+var equation = 0;
+while (question < 5) {
+    var a = Math.floor(Math.random()*10-3)+3;
+    var b = Math.floor(Math.random()*10-3)+3;
+// Set a variable called product to be the product of a and b
+    var product = a * b;
+equation = alert(a + " * " + b + "= ??");
+// Ask the equation of the user and store the user input in a variable called answer
+answer = prompt(equation);
+question++;
+}
+// Say "Correct!" if the answer and product match, "Incorrect!" if not.
+if (answer == product) alert("Correct!");
+else alert("Incorrect!");
+/* Multiplication whjile Loop */
+// Embed all this code in a While loop and run it 5 times.
+
+/* Multiplication For Loop */
+// Convert the while loop to a for loop. 
