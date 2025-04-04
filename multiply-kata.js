c710650c42f1bd637127cd82525d18b14cd80b52
Anastasiia809 <anastasiia208805@student-schoolsofwestfield.org>
2025-04-04 14:49:35 +0000

    done 2nd time

--- multiply-kata.js
+++ multiply-kata.js
@@ -1,19 +1,7 @@
 /* Multiplication */
 
 // Create 2 variables, a & b, and set to ne random integers between 3 and 9
-// Set a variable called equation to display  a * b = ??
-var equation = 0;
-for (let question = 0; question < 5; question++) {
-    var a = Math.floor(Math.random()*(10-3))+3;
-    var b = Math.floor(Math.random()*(10-3))+3;
-// Set a variable called product to be the product of a and b
-    var product = a * b;
-equation = alert(a + " * " + b + " = ??");
-// Ask the equation of the user and store the user input in a variable called answer
-answer = prompt(equation);
-if (answer == product) alert("Correct!");
-else alert("Incorrect!");
-}
+// Set a variable called equation to display  a * b = ?
 // Say "Correct!" if the answer and product match, "Incorrect!" if not.
 /* Multiplication whjile Loop */
 // Embed all this code in a While loop and run it 5 times.
--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 /* Multiplication */
 
 // Create 2 variables, a & b, and set to ne random integers between 3 and 9
-// Set a variable called equation to display  a * b = ??
 var equation = 0;
-for (let question = 0; question < 5; question++) {
-    var a = Math.floor(Math.random()*(10-3))+3;
-    var b = Math.floor(Math.random()*(10-3))+3;
-// Set a variable called product to be the product of a and b
-    var product = a * b;
-equation = alert(a + " * " + b + " = ??");
-// Ask the equation of the user and store the user input in a variable called answer
-answer = prompt(equation);
-if (answer == product) alert("Correct!");
-else alert("Incorrect!");
+var answer = 0;
+for (let question = 0;question < 5; question++) {
+    let a = Math.floor(Math.random()*7)+3;
+    let b = Math.floor(Math.random()*7)+3;
+    let product = a* b;
+    equation = alert(a + "*" + b + " = ?");
+    answer = prompt(equation);
+    if (answer == product) alert("Correct!");
+    else alert("Incorrect!");
 }
+// Set a variable called equation to display  a * b = ?
 // Say "Correct!" if the answer and product match, "Incorrect!" if not.
 /* Multiplication whjile Loop */
 // Embed all this code in a While loop and run it 5 times.
